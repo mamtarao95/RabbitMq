49e6a21a1c5638191657438ea83c351637150f25
mamtarao95 <mamtarao9395@gmail.com>
2018-07-16 19:34:39 +0530

    rabbitmq example

---
+++ src/main/java/com/bridgelabz/springrabbitmq/controllers/RabbitMQWebController.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.springrabbitmq.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.springrabbitmq.model.Employee;
+import com.bridgelabz.springrabbitmq.services.RabbitMQSender;
+
+
+@RestController
+public class RabbitMQWebController {
+
+	@Autowired
+	RabbitMQSender rabbitMQSender;
+
+	@GetMapping(value = "/producer")
+	public String producer(@RequestParam("empName") String empName,@RequestParam("empId") String empId) {
+	
+	Employee emp=new Employee();
+	emp.setEmpId(empId);
+	emp.setEmpName(empName);
+		rabbitMQSender.send(emp);
+
+		return "Message sent to the RabbitMQ JavaInUse Successfully";
+	}
+
+}
+
